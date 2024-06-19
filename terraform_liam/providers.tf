ae1a026e818267a768bdd041f6825bf0e2cd68f3
Liam-mirissa <liam.mirissa@gmail.com>
2024-06-19 17:10:18 +0000

    made changes to  k8

--- terraform_liam/providers.tf
+++ terraform_liam/providers.tf
@@ -2,3 +2,12 @@ provider "aws" {
   region                   = "us-east-2"
   shared_credentials_files = ["~/.aws/credentials"]
 }               
+
+terraform {
+  backend "s3" {
+    bucket = "backendtfliam"
+    key = "statefile/terraform.tfstate"
+    region = "us-east-2"
+    
+  }
+}
